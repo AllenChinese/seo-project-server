0c3f48f2e0d307b131a3dcc583c48821fac975fb
AllenChinese <13402202685@163.com>
2018-12-04 16:33:49 +0800

    feat: helper  新增 getUrlParams 方法

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,9 +1,18 @@
 // lodash 工具库 辅助 api 数据处理
 'use strict';
 const _get = require('lodash').get;
+const qs = require('qs');
 
 module.exports = {
   getData(obj, path, defaultValue = '') {
     return _get(obj, path, defaultValue);
+  },
+  getUrlParams(string = '') {
+    const paramString = string;
+    const reg = /\?(.+)/;
+    const result = paramString.match(reg);
+    return result
+      ? qs.parse(result[1])
+      : '';
   },
 };
